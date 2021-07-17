c826965bd0e5723c842cdca4fb3285f14d06d364
kraftdorian <work.kajzer.damian@gmail.com>
2021-07-18 01:25:18 +0200

    Chore:- Installed React, added peer deps, updated file.

--- src/index.ts
+++
@@ -1,3 +0,0 @@
-const Component = () => {};
-
-export default Component;
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import {PropsWithChildren} from "react";
+
+const Component: React.FC<{}> = ({ children }) => {
+  return <div>{children}</div>
+};
+
+export default Component;
