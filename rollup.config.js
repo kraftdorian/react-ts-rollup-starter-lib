31036839cfcf2fef40934b5ec2933b634c9d4c3d
kraftdorian <work.kajzer.damian@gmail.com>
2021-07-18 01:18:24 +0200

    Chore:- Installed deps, created configuration and first file.

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import babel from "@rollup/plugin-babel";
+import resolve from "@rollup/plugin-node-resolve";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import pkg from "./package.json";
+
+// Array of extensions to be handled by babel
+const EXTENSIONS = [".ts", ".tsx"];
+
+// Excluded dependencies
+const EXTERNAL = Object.keys(pkg.devDependencies);
+
+export default {
+  input: ["src/index.ts"],
+  output: {
+    dir: "dist",
+    sourcemap: true,
+    format: "esm",
+    preserveModules: true
+  },
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    babel({
+      extensions: EXTENSIONS,
+      babelHelpers: "inline",
+      include: EXTENSIONS.map(ext => `src/**/*${ext}`)
+    })
+  ],
+  external: EXTERNAL
+};
